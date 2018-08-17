8ad156ca016fa97d4776e020a5579c5d68bdbc67
Travis <Travisagrossman@gmail.com>
2018-08-17 13:25:33 -0700

    connecting and esablishing DB

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # butikk-liste
+
+An efficient shopping list built to practice some MERN stack necessitates.
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.export = {
+  mongoURI:
+    "  mongodb://Travis:travis123@ds123532.mlab.com:23532/g25-mern-shopping-list"
+};
