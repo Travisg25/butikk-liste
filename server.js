d183f61700f40eca30f017b4af75d9c197621afc
Travis <Travisagrossman@gmail.com>
2018-08-28 12:24:50 -0700

    adding routes for items

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const items = require("./routes/api/items.js");
 const app = express();
 
 //Bodyparser Middleware
 app.use(bodyParser.json());
 
 // Db Config
-const db = reuire("./config/keys").mongoURI;
+const db = require("./config/keys").mongoURI;
 // Connect to Mongo
 mongoose
-  .connect(db)
+  .connect(
+    "mongodb://Travis:travis123@ds123532.mlab.com:23532/g25-mern-shopping-list"
+  )
   .then(() => console.log("Mongo Connected..."))
   .catch(err => console.log(err));
+
+app.use("api/items", items);
 
 const port = process.env.PORT || 5000;
 
