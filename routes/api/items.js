921e18cf452b26ceb72c624d1a460a1372b672e9
Travis <Travisagrossman@gmail.com>
2018-08-29 13:09:43 -0700

    end points set up post delete

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,9 +4,24 @@ const router = express.Router();
 //Item Model
 const Item = require("../../models/Item");
 
-//@ route GET
-//@ description Get all items
+//@ route POST api/items
+//@ description Create an item
 //@ access Public
 
-router.get("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  newItem.save().then(item => res.json(item));
+});
+
+//@ route DELETE api/items/:id
+//@ description Delete a item
+//@ access Public
+
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status((404).json({ success: false })));
+});
 module.exports = router;
