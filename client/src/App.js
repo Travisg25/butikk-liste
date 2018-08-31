e5a8941ef8e3bf6bf364bd6dba506cf4caca3461
Travis <Travisagrossman@gmail.com>
2018-08-31 10:48:05 -0700

    items added through state able to add/delete

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Hello</h1>
         <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
